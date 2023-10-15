f8c8742b6fb6f82f8de200bc6939ad5f51441e8c
TorySnopl <Paramedic064@gmail.com>
2023-10-14 20:31:08 -0700

    need to add query for update employee role but otherwise functional

--- server.js
+++ server.js
@@ -62,6 +62,7 @@ console.log(choice)
         showDb();
       });
       break;
+
     case 'Add an Employee':
       inq.prompt([
         {
@@ -102,29 +103,98 @@ console.log(choice)
         console.error(err);
       })
       break;
-    case 'Update Employee Role':
-      console.log('Updated employee role');
-      showDb();
+
+    case 'Update Employee Role': //Working here
+      inq.prompt([
+        {
+          type: 'input',
+          name: 'eID',
+          message: 'What is the ID of the employee you would you like to update?'
+        },
+        {
+          type: 'input',
+          name: 'newRoleId',
+          message: 'What is the new Role ID for the employee?'
+        }
+      ]).then((answers)=>{
+        console.log(answers);
+        showDb();
+      }).catch((err)=>{
+        console.error(err);
+      });
       break;
+
     case 'View all Roles':
       db.query('SELECT * FROM job', function(err,results){
         console.log(results);
         showDb()
       });
       break;
+
     case 'Add Role':
-      console.log('Added a role');
-      showDb();
+      inq.prompt([
+        {
+          type: 'input',
+          name: 'title',
+          message: 'what is the title of the new role?'
+        },
+        {
+          type: 'input',
+          name: 'salary',
+          message: 'what is the salary for this role?'
+        },
+        {
+          type: 'list',
+          name: 'deptID',
+          message: 'what is the department ID of the new role?',
+          choices: ['001','002','003','004']
+        }
+      ]).then((answers)=>{
+        db.query(`INSERT INTO job (r_title, salary, department_id) VALUES (?,?,?)`,[answers.title, answers.salary, answers.deptID], function (err, results){
+          if (err) throw err;
+          if (results.affectedRows>0){
+            console.log(`${answers.title} was successfully added`);
+            showDb();
+          } else {
+            console.log("No changes were made to the database");
+            showDb();
+          }
+        })
+        
+      }).catch((err)=>{
+        console.error(err);
+      })
       break;
+    
     case 'View all Departments':
       db.query('SELECT * FROM department', function(err,results){
         console.log(results);
         showDb()
       });
       break;
+
     case 'Add Department':
-      console.log('Added a department');
-      showDb();
+      inq.prompt([
+        {
+          type: 'input',
+          name: 'title',
+          message: 'what is the name of the new department?'
+        }
+      ]).then((answers)=>{
+        db.query(`INSERT INTO department (d_name) VALUES (?)`,[answers.title], function (err, results){
+          if (err) throw err;
+          if (results.affectedRows>0){
+            console.log(`${answers.title} was successfully added`);
+            showDb();
+          } else {
+            console.log("No changes were made to the database");
+            showDb();
+          }
+        })
+        
+      }).catch((err)=>{
+        console.error(err);
+      })
       break;
   }
 
